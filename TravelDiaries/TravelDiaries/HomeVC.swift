28c6f67393d31aa50910e3ed3c3d20d4c100d940
Pranav V K <Pranav.k28@wipro.com>
2018-09-03 17:24:53 +0530

    + Removed story board and installed Alamofire podfile

---
+++ TravelDiaries/TravelDiaries/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TravelDiaries
+//
+//  Created by Wipro on 03/09/18.
+//  Copyright © 2018 Wipro. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+    func setupHomePage()
+    {
+
+    }
+
+    func invokeWebServiceAPI()
+    {
+        
+    }
+
+
+}
+
---
+++ TravelDiaries/TravelDiaries/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  TravelDiaries
+//
+//  Created by Wipro on 03/09/18.
+//  Copyright © 2018 Wipro. All rights reserved.
+//
+
+import Foundation
+
+func makeWebServiceCall() {
+
+    
+}
